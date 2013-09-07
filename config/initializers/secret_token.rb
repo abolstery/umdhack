ad4be7abeb82a0df90983720ac3ba4a72880d600
Aboli Kumthekar <abolikumthekar@gmail.com>
2013-09-07 19:24:34 -0400

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackathon::Application.config.secret_key_base = '84f529f15913340a90b265a654b00c457b7cd818f2eb5322a2fc8ffb6183e9c5d3556651de8308060d9c4cbfa21d35c62a0be1689c43685d6d4b7b43abd42688'
